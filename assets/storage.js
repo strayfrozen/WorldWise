356abf8714f5272178a732c11be6058396ac0a39
Stray <strayfrozen@yahoo.com>
2021-11-20 20:31:19 -0600

    added a display data function that appends data from local storage still in progress

--- assets/storage.js
+++ assets/storage.js
@@ -1,3 +1,5 @@
+ var countryContainer = document.getElementById('countryContainer')
+ 
  //This code collects user input and stores it into local storage 
  let Countries = [];
 
@@ -14,8 +16,45 @@ document.querySelector('form').reset();
 localStorage.setItem('MyCountryList', JSON.stringify(Countries) );
 console.warn('added' , {Countries} );
 
+
+//This code appends localStorage data to page
+
+let displayData = ()=>{
+    var info = JSON.parse(localStorage.getItem('MyCountryList'));
+   // for (var i = 0; i < info.length; i++){
+   // console.log('Country Name: ', JSON.parse(info));
+    var countryCard = document.createElement('div')
+    countryCard.setAttribute('class', 'card')
+    countryContainer.append(countryCard)
+    
+    var countryName = document.createElement('h3')
+            countryName.textContent = 'Country Name: ' + info
+            countryCard.prepend(countryName)
+    //   }
+    }
+    
+    displayData();
+
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
 document.getElementById("btn").addEventListener('click', addToStorage);
 });
+
+
+/*//This code appends localStorage data to page
+
+let displayData = ()=>{
+var info = localStorage.getItem('MyCountryList', JSON.stringify(Countries) );
+console.log(info);
+var countryCard = document.createElement('div')
+countryCard.setAttribute('class', 'card')
+countryContainer.append(countryCard)
+
+var countryName = document.createElement('h3')
+        countryName.textContent = 'Country Name: ' + info
+        countryCard.prepend(countryName)
+
+}
+
+displayData();*/
