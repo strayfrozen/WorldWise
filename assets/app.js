a4f192f57a087c0e2e41552e2caa9b0f675a36aa
David Opoku <71300793+dopoku12@users.noreply.github.com>
2021-11-24 15:51:32 -0500

    search added

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,22 @@ search.addEventListener('click', function (event) {
     const input = document.querySelector('#search').value
 
     fetchData(input)
+});
+
+async function fetchData(country) {
+    let response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+    let data = await response.json()
+    console.log(data)
+    // let detailUrl = data.deaths.detail
+
+    // let detailResponse = await fetch(detailUrl)
+    // let detailData = await detailResponse.json()
+    // console.log('detailData: ', detailData);
+    // console.log(detailData[0]);
+    // var dataElem = document.createElement('div')
+    // dataElem.innerHTML = detailData[0].deaths
+    // document.body.appendChild(dataElem)
+
+};
+// const search = window.prompt()
+console.log(fetchData('mexico'))
