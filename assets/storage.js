7aabd691c2269b12fef5caf1f52960d734b444f1
Stray <strayfrozen@yahoo.com>
2021-11-24 15:34:14 -0600

    Fixed local storage bug and created clickable bookmarks

--- assets/storage.js
+++ assets/storage.js
@@ -1,47 +1,69 @@
- var countryContainer = document.getElementById('countryContainer')
- 
- //This code collects user input and stores it into local storage 
- let Countries = [];
+var APIUrl = 'https://restcountries.com/v3.1/name/{countryname}'
 
-const addToStorage = (ev)=>{
+var countryContainer = document.getElementById('countryContainer')
+
+//This code collects user input and stores it into local storage 
+
+const addToStorage = (ev) => {
     ev.preventDefault();
-let userInfo = {
-   // id: Date.now(),
-    Country: document.getElementById("search").value
-}
-//let userInfo = document.getElementById('search').value
-Countries.push(userInfo);
-document.querySelector('form').reset();
-console.log(Countries)
-
-//saving to localStorage
-localStorage.setItem('MyCountryList', JSON.stringify(Countries) );
-console.warn('added' , {Countries} );
-
-
-//This code appends localStorage data to page
-
-let displayData = ()=>{
-    countryContainer.innerHTML = '';
-    var info = JSON.parse(localStorage.getItem('MyCountryList'));
-   // for (var i = 0; i < info.length; i++){
-   // console.log('Country Name: ', JSON.parse(info));
-    var countryCard = document.createElement('div')
-    countryCard.setAttribute('class', 'card')
-    countryContainer.append(countryCard)
-    for (var i = 0; i < info.length; i++){
-    var countryName = document.createElement('h3')
-            countryName.textContent = 'Country Name: ' + info[i].Country
-            countryCard.prepend(countryName)
-       }
+    var Countries = JSON.parse(localStorage.getItem('MyCountryList'));
+    if (Countries === null) {
+        Countries = []
     }
-    
+    let userInfo = {
+        // id: Date.now(),
+        Country: document.getElementById("search").value
+    }
+    //let userInfo = document.getElementById('search').value
+    Countries.push(userInfo);
+    document.querySelector('form').reset();
+    console.log(Countries)
+
+    //saving to localStorage
+    localStorage.setItem('MyCountryList', JSON.stringify(Countries));
+    console.warn('added', { Countries });
+
+
+    //This code appends localStorage data to page
+
+
     displayData();
 
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-document.getElementById("btn").addEventListener('click', addToStorage);
+let displayData = () => {
+    countryContainer.innerHTML = '';
+    var info = JSON.parse(localStorage.getItem('MyCountryList'));
+    // for (var i = 0; i < info.length; i++){
+    // console.log('Country Name: ', JSON.parse(info));
+    var countryCard = document.createElement('div')
+    countryCard.setAttribute('class', 'card')
+    countryContainer.append(countryCard)
+    for (var i = 0; i < info.length; i++) {
+        var countryName = document.createElement('h3')
+        countryName.setAttribute('id', info[i].Country)
+        countryName.textContent = 'Country Name: ' + info[i].Country
+        countryCard.prepend(countryName)
+
+        countryName.addEventListener('click', function (event) {
+            var clickedId = event.target.id;
+            getCountryData(clickedId)
+        })
+    }
+}
+
+displayData()
+
+function getCountryData(id) {
+    fetch(APIUrl + id)
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log('data', data);
+        })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btn").addEventListener('click', addToStorage);
 });
 
 
