892a2d5da4e423185a449f547fa4ba748daa289e
Stray <strayfrozen@yahoo.com>
2021-11-23 21:11:41 -0600

    updated data function

--- assets/storage.js
+++ assets/storage.js
@@ -4,13 +4,15 @@
  let Countries = [];
 
 const addToStorage = (ev)=>{
-ev.preventDefault();
+    ev.preventDefault();
 let userInfo = {
-    id: Date.now(),
+   // id: Date.now(),
     Country: document.getElementById("search").value
 }
+//let userInfo = document.getElementById('search').value
 Countries.push(userInfo);
 document.querySelector('form').reset();
+console.log(Countries)
 
 //saving to localStorage
 localStorage.setItem('MyCountryList', JSON.stringify(Countries) );
@@ -20,17 +22,18 @@ console.warn('added' , {Countries} );
 //This code appends localStorage data to page
 
 let displayData = ()=>{
+    countryContainer.innerHTML = '';
     var info = JSON.parse(localStorage.getItem('MyCountryList'));
    // for (var i = 0; i < info.length; i++){
    // console.log('Country Name: ', JSON.parse(info));
     var countryCard = document.createElement('div')
     countryCard.setAttribute('class', 'card')
     countryContainer.append(countryCard)
-    
+    for (var i = 0; i < info.length; i++){
     var countryName = document.createElement('h3')
-            countryName.textContent = 'Country Name: ' + info
+            countryName.textContent = 'Country Name: ' + info[i].Country
             countryCard.prepend(countryName)
-    //   }
+       }
     }
     
     displayData();
