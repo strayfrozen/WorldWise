ca89986d16bb8ded0c13d83ac3843d6beb089927
David Opoku <71300793+dopoku12@users.noreply.github.com>
2021-11-28 04:01:37 -0500

    removed ip address & added leaflet marker comment

--- assets/js/mapScript.js
+++ assets/js/mapScript.js
@@ -1,32 +1,31 @@
+
+
+
+//User Ip address
 async function fetchIPData() {
-    let response = await fetch('https://api.ipregistry.co/?key=tryout')
-    console.log(response.url);
-    let userData = await response.json()
-    console.log(userData);
-    let coordLat = userData.location.latitude
-    let coordLong = userData.location.longitude
-    console.log(coordLat, coordLong);
-    const countryIp = userData.location.country.name;
-    console.log(countryIp);
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude } = position.coords
+        const { longitude } = position.coords
+
+
+        //leafletMap
+        const map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        let userMarker = L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('You are here')
+            .openPopup();
 
 
 
 
+        map.panTo(new L.LatLng());
 
 
+    });
 
 
-    const map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-
-    map.panTo(new L.LatLng(coordLat, coordLong));
-}
-
-fetchIPData()
+    fetchIPData()
